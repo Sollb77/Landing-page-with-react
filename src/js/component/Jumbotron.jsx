0c7b2c6bc447db322e43c46e64b0300935046c1a
Sollb77 <qsiecuadorservicios@gmail.com>
2023-01-26 15:37:47 +0000

    final

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -7,8 +7,8 @@ import React from "react";
 
 const Jumbotron = () => {
 	return(
-		<div className="col-md-6">
-        <div className="h-100 p-12 bg-light border rounded-3">
+		<div className="col-md-11 m-3 p-3 ms-5 ">
+        <div className="h-300 m-3 p-3 bg-light border rounded-3">
           <h1>A Warm Welcome</h1>
           <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be sure to look under the hood at the source HTML here as we've adjusted the alignment and sizing of both column's content for equal-height.</p>
           <button className="btn btn-outline-secondary" type="button">Call to action</button>
